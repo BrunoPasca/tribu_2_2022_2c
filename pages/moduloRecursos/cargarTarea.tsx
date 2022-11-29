b3442d83c977c8c838e61a2fa28890b177d409a9
BrunoPasca <42937086+BrunoPasca@users.noreply.github.com>
2022-11-29 19:46:32 -0300

    [soporte] adds table for tasks

--- pages/moduloRecursos/cargarTarea.tsx
+++ pages/moduloRecursos/cargarTarea.tsx
@@ -5,6 +5,7 @@ import styles from '../../styles/recursos.module.css'
 import Header from '../header';
 import SeleccionarActividad from "./seleccionarActividad";
 import { getProyectos, getTareasByProyecto } from "./services/ProyectoService";
+import MuiTable from "./tablaHoras";
 
 export default function CargarTarea() {
     const [fecha, setFecha] = React.useState()
@@ -21,6 +22,8 @@ export default function CargarTarea() {
     const [fechaInicio, setFechaInicio] = React.useState(new Date())
     const [fechaFin, setFechaFin] = React.useState(new Date())
 
+    const [periodo, setPeriodo] = React.useState("Semanal")
+
     useEffect(() => {
         // Recupero los datos
         if (typeof window !== "undefined") {
@@ -31,10 +34,10 @@ export default function CargarTarea() {
 
         getProyectos().then((data) => {
             setProyectos(data);
-          })
-          .catch(function (ex) {
-              console.log('Response parsing failed. Error: ', ex);
-          });;
+        })
+            .catch(function (ex) {
+                console.log('Response parsing failed. Error: ', ex);
+            });;
 
         if (!proyectos[0]) return;
         setProyectoId(proyectos[0].id)
@@ -45,22 +48,21 @@ export default function CargarTarea() {
         if (!proyectoId) return;
         getTareasByProyecto(proyectoId).then((data) => {
             setTareas(data);
-          })
-          .catch(function (ex) {
-              console.log('Response parsing failed. Error: ', ex);
-          });;
+        })
+            .catch(function (ex) {
+                console.log('Response parsing failed. Error: ', ex);
+            });;
     }, [proyectoId])
 
-    return(
+    return (
         <div>
             <Header></Header>
             <div className={styles.cargarTarea}>
                 <div className={styles.ingresarInfoTarea}>
-                    <SeleccionarActividad actividad="Tarea"/>
-
+                    <SeleccionarActividad actividad="Tarea" />
                     <label className={styles.inputLabel}>Proyecto</label>
-                    <select 
-                        id="proyecto" 
+                    <select
+                        id="proyecto"
                         className={styles.selectInput}
                         value={proyectoId}
                         onChange={(e) => {
@@ -68,56 +70,62 @@ export default function CargarTarea() {
                         }}
                         name="actividad"
                     >
-                    {
-                        proyectos.map(proyecto =>
-                            <option key={proyecto.id} value={proyecto.id}>{proyecto.nombre}</option>  
-                        )
-                    }  
-                    </select> 
+                        {
+                            proyectos.map(proyecto =>
+                                <option key={proyecto.id} value={proyecto.id}>{proyecto.nombre}</option>
+                            )
+                        }
+                    </select>
                     <br></br>
                     <br></br>
 
                     <label className={styles.inputLabel}>Tarea</label>
-                    <select 
-                        id="tarea" 
+                    <select
+                        id="tarea"
                         className={styles.selectInput}
                         value={tareaId}
                         onChange={(e) => {
                             setTareaId(e.currentTarget.value)
-                          }}
+                        }}
                         name="actividad"
                     >
-                    {
-                        tareas.map(tarea =>
-                            <option key={tarea.id} value={tarea.id}>{tarea.descripcion}</option>  
-                        )
-                    }  
-                    </select> 
+                        {
+                            tareas.map(tarea =>
+                                <option key={tarea.id} value={tarea.id}>{tarea.descripcion}</option>
+                            )
+                        }
+                    </select>
                     <br></br>
                     <br></br>
 
                     <div className={styles.calendarInput}>
                         <label className={styles.inputLabel}>Fecha</label>
-                        <DatePicker className={styles.datePicker} selected={fecha} onChange={(date : any) => setFecha(date)}
-                                    minDate={fechaInicio} maxDate={fechaFin} 
+                        <DatePicker className={styles.datePicker} selected={fecha} onChange={(date: any) => setFecha(date)}
+                            minDate={fechaInicio} maxDate={fechaFin}
                         />
                     </div>
                     <br></br>
 
                     <div className={styles.flexContainer}>
                         <label className={styles.smallInputLabel}>De</label>
-                        <DatePicker className={styles.datePicker} dateFormat="h aa" showTimeSelect showTimeSelectOnly selected={horaInicio} onChange={(date:any) => setHoraInicio(date)}/>
+                        <DatePicker className={styles.datePicker} dateFormat="h aa" showTimeSelect showTimeSelectOnly selected={horaInicio} onChange={(date: any) => setHoraInicio(date)} />
                         <label className={styles.smallInputLabel}>a</label>
-                        <DatePicker className={styles.datePicker} dateFormat="h aa" showTimeSelect showTimeSelectOnly selected={horaFin} onChange={(date:any) => setHoraFin(date)}/>
-                    </div>                
+                        <DatePicker className={styles.datePicker} dateFormat="h aa" showTimeSelect showTimeSelectOnly selected={horaFin} onChange={(date: any) => setHoraFin(date)} />
+                    </div>
                     <div className={styles.containerBotones}>
                         <button>Cancelar</button>
                         <button>Cargar Tarea</button>
                     </div>
-
                 </div>
-            </div>            
+                <div className={styles.ingresarInfoTarea}>
+                    <div className={styles.holder}>
+                        <label className={styles.titleLabel}>Actividades Cargadas</label>
+                        <label className={styles.titleLabel}>Periodo: {periodo}</label>
+                        <MuiTable />
+                    </div>
+                </div>
             </div>
+        </div>
 
     )
 }
