6fdb127355cb96c31cf5bfc84f169dc56a37791d
Tomas <tomalon95@gmail.com>
2022-12-01 17:49:35 -0300

    Cambio a eliminacion de proyectos

--- pages/moduloProyectos/eliminarProyecto/[id].tsx
+++ pages/moduloProyectos/eliminarProyecto/[id].tsx
@@ -17,6 +17,7 @@ export default function eliminarProyecto(){
         <Head_ nombre='Eliminar proyecto'></Head_>
         <Header></Header>
         <main className={styles.main}>
+            <form method = "GET" action = {"https://aninfo2c222back-production.up.railway.app/api/proyectos/"+id}>
             <div className={styles.container}>
                 <div className={styles.confirmacion}>
                     <h1 className={styles.tituloForm}>¡Atención!</h1>
@@ -26,9 +27,11 @@ export default function eliminarProyecto(){
 
                 <div className={styles.botonesView}>
                     <button onClick={function_button}>Confirmar</button>
-                    <a href="/moduloProyectos/proyectoView"><button>Cancelar</button></a>
+                    <a href="/moduloProyectos/proyectoView"><button type = "submit">Cancelar</button></a>
                 </div>
             </div>
+        </form>
+
         </main>
         </>
     )
