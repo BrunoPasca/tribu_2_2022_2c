148631a22b92ba524e88fef03f33f9093b6817ef
LucianaH <lhoszowski@fi.uba.ar>
2022-12-01 13:59:21 -0300

    cosas

--- pages/moduloRecursos/cards/licenciaCard.tsx
+++ pages/moduloRecursos/cards/licenciaCard.tsx
@@ -5,7 +5,7 @@ export default function licenciaCard(props : any) {
     return(
         <div className={styles.reporteCard}>
             <div className={styles.tituloCard}>{props.tipo_licencia}</div>
-            <div className={styles.tituloCard}>{props.fecha_inicio} a {props.fecha_fin}</div>
+            <div className={styles.severidadCard}>{props.fecha_inicio} a {props.fecha_fin}</div>
         </div>
     );
   }
