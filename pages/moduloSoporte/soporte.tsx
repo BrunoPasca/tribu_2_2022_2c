6ea742296dd2f21b909977e20ee444c3d83de21c
MateoF01 <mateofernandez380@gmail.com>
2022-12-01 14:59:12 -0300

    SEARCH BARS

--- pages/moduloSoporte/soporte.tsx
+++ pages/moduloSoporte/soporte.tsx
@@ -6,7 +6,7 @@ import ColumnaTicket from './columnaTickets';
 import SearchBar from './searchBar';
 import SearchBarResponsable from './searchBarResponsable';
 import TicketData from './TicketsData.json'
-import SearchBarSeveridad from './searchBar';
+import SearchBarSeveridad from './searchBarSeveridad';
 import SearchBarLegajoCliente from './searchBarLegajoCliente';
 
 
@@ -25,14 +25,14 @@ export default function Soporte() {
           </div>
           <div className={styles.navbarDer}> 
           
-            
-            <SearchBarSeveridad placeholder="Ingrese una Severidad" data={TicketData}/>
-
             <SearchBarResponsable placeholder='Ingrese id de resposable'data={TicketData} ></SearchBarResponsable>
 
             <SearchBar placeholder="Ingrese un Titulo" data={TicketData}/>
             
             <SearchBarLegajoCliente placeholder="Ingrese un id de cliente" data={TicketData}/>
+
+            <SearchBarSeveridad placeholder='Ingrese una severidad' data={TicketData}></SearchBarSeveridad>
+
 
           </div>
         </div>
