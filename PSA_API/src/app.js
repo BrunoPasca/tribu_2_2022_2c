46b76d55aa5b85af86c5d74b8a71d351d8c9322f
OliGruss <72172817+OliGruss@users.noreply.github.com>
2022-11-30 21:02:07 -0300

    Importo los routes a app.js

--- PSA_API/src/app.js
+++ PSA_API/src/app.js
@@ -1,11 +1,23 @@
 import express from "express";
 import ticketsRoutes from "./routes/tickets.routes.js";
+import employeesRoutes from "./routes/employees.routes.js";
+import horasRoutes from "./routes/horas.routes.js";
+import faltasRoutes from "./routes/faltas.routes.js";
+import guardiasRoutes from "./routes/guardias.routes.js";
+import licenciaRoutes from "./routes/licencia.routes.js";
+import projectsRoutes from "./routes/projects.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api", ticketsRoutes);
+app.use("/api", employeesRoutes);
+app.use("/api", horasRoutes);
+app.use("/api", faltasRoutes);
+app.use("/api", guardiasRoutes);
+app.use("/api", licenciaRoutes);
+app.use("/api", projectsRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({
