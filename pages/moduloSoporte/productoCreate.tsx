3538604f53d163456ce085622769f3cf2e5a28d2
MateoF01 <mateofernandez380@gmail.com>
2022-12-04 14:40:34 -0300

    Revert "borrando cosas"
    This reverts commit 4904698428c5e4ac465d701bdd352e36c00b3298.

--- pages/moduloSoporte/productoCreate.tsx
+++ pages/moduloSoporte/productoCreate.tsx
@@ -7,6 +7,7 @@ import { useForm } from 'react-hook-form';
 import Link from 'next/link';
 import { useNavigate } from 'react-router-dom';
 import { versions } from 'process';
+import VersionForm from './versionForm';
 
 
 
