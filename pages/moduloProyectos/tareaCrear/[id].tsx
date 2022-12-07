4cc9dbfb6f4e344726ee0389644a3747d74d2325
Julieta <perezg.julieta@gmail.com>
2022-12-07 12:35:37 -0300

    arreglo vista crear tarea

--- pages/moduloProyectos/tareaCrear/[id].tsx
+++ pages/moduloProyectos/tareaCrear/[id].tsx
@@ -95,72 +95,65 @@ export default function crearTarea(){
 
     return (
         <>
-        <Head_ nombre='Nueva tarea'></Head_>
+        <Head_ nombre='Crear tarea'></Head_>
         <Header></Header>
         <main className={styles.main}>
-        <div className={styles.proyectoView}>
-                <h1 className={styles.tituloEdit}>Crear tarea en proyecto {id}</h1>
-                <div className={styles.contenedorPadre}>
-                    <div className={styles.infoProyecto}>
-                        <div className={styles.tituloInfo}>
-                            <div>Proyecto</div>
-                            <div>Estado</div>
-                            <div>Descripción</div>
-                            <div>Horas estimadas</div>
-                            <div>Prioridad</div>
-                            <div>Fecha inicio</div>
-                            <div>Legajo del recurso</div>
-                        </div>
-                        <div className={styles.info} >
-                            <form  id = "form_id" onSubmit = {onSubmit}>
-                            <select id = "id proyecto" {...register("id_proyecto")}>
+        <div className={styles.containerEspecial}>
+          <form  id = "form_id" onSubmit = {onSubmit}>
+            <div className={styles.camposForm}>
+                <h1 className={styles.tituloForm}>Nueva tarea en proyecto {id}</h1>
+                <div >
+                        <label htmlFor="fname">Proyecto</label><br />
+                        <select id = "id proyecto" {...register("id_proyecto")}>
                             {(proyectos.filter(element => element.id == Number(id)).map( (proyecto) =>
                                 <option value = {proyecto?.id}> {proyecto?.id} </option>))}
-                            </select>
-                            <div>
-                            <select id = "estado_tarea" {...register("estado")}>
-                                <option value="Abierta">Abierta</option>
-                                <option value="En análisis">En análisis</option>
-                                <option value="En progreso">En progreso</option>
-                                <option value="Suspendida">Suspendida</option>
-                                <option value="Completada">Completada</option>
-                            </select>
-                            </div>
-                                <input type="text" id="descripcion" {...register("descripcion")} placeholder="Descripcion" size={50}></input>
-
-                                <input type="number" id="horasEstimadas" {...register("horas_estimadas")} min="0"></input>
-                                <div>
-                                    <select id="prioridad_tarea" {...register("prioridad")}>
-                                        <option  value = "Baja"> Baja</option>
-                                        <option  value = "Media"> Media</option>
-                                        <option  value = "Alta">Alta</option>
-                                    </select>
-                                </div>
-                            <div>
-                                <input type = "date" id="fecha_inicio" {...register("fecha_inicio")}></input>
-                            </div>
-
-                            <select id = "legajo_id" {...register("legajo_recurso")}>
-                            {(recursos.map( (recurso) =>
-                                <option value = {recurso?.legajo}> {recurso?.Nombre}  {recurso?.Apellido} </option>))}
-                            </select>
-
-                            <select >
-                            {(tareas.filter(element => element.id_proyecto == Number(id)).map( (tarea) =>
-                                <option value = {tarea?.id}> {tarea?.id} </option>))}
-                            </select>
-                            </form>
+                        </select>
+                        <div>
+                        <label htmlFor="fname">Estado</label><br />
+                          <select id = "estado_tarea" {...register("estado")}>
+                            <option value="Abierta">Abierta</option>
+                            <option value="En análisis">En análisis</option>
+                            <option value="En progreso">En progreso</option>
+                            <option value="Suspendida">Suspendida</option>
+                            <option value="Completada">Completada</option>
+                          </select>
                         </div>
-                    </div>
-                </div>
+                        <label htmlFor="fname">Descripción</label><br />
+                        <input type="text" id="descripcion" {...register("descripcion")} placeholder="Descripcion" size={50}></input><br />
+                        <label htmlFor="fname">Horas estimadas</label><br />
+                        <input type="number" id="horasEstimadas" {...register("horas_estimadas")} min="0"></input>
+                        <div>
+                          <label htmlFor="fname">Prioridad</label><br />
+                          <select id="prioridad_tarea" {...register("prioridad")}>
+                            <option  value = "Baja"> Baja</option>
+                            <option  value = "Media"> Media</option>
+                            <option  value = "Alta">Alta</option>
+                          </select>
+                        </div>
+                        <div>
+                          <label htmlFor="fname">Fecha de inicio</label><br />
+                          <input type = "date" id="fecha_inicio" {...register("fecha_inicio")}></input>
+                        </div>
+                        <label htmlFor="fname">Recurso asignado</label><br />
+                        <select id = "legajo_id" {...register("legajo_recurso")}>
+                          {(recursos.map( (recurso) =>
+                          <option value = {recurso?.legajo}> {recurso?.Nombre}  {recurso?.Apellido} </option>))}
+                        </select>
 
-        <div className={styles.botonesView}>
-        <a href={"/moduloProyectos/proyectTareaView/"+id}><button>Cancelar</button></a>
-        <a href = {"/moduloProyectos/proyectTareaView/"+id}>
-        <button type="submit" form="form_id" >Crear</button>
-        </a>
-        </div>
-        </div>
+                           
+                        </div>
+                    
+              
+
+              <div className={styles.botonesView}>
+                <a href={"/moduloProyectos/proyectTareaView/"+id}><button>Cancelar</button></a>
+                <a href = {"/moduloProyectos/proyectTareaView/"+id}>
+                <button type="submit" form="form_id" >Crear</button>
+                </a>
+              </div>
+              </div>
+            </form>
+          </div>
         </main>
         </>
     )
